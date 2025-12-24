37fb7612953e1d646c8b4f629c9f2c5ffee049bd
LandonScottBridgewater <trios-recap6f@icloud.com>
2025-12-24 17:37:06 -0600

    Spyder_E no longer saves recent projects.

--- readme.md
+++ readme.md
@@ -2,25 +2,22 @@
 
 Spyder-E is a variation of Spyder which does not store any project-sensitive information data on disk.
 
-# Installation
+[Vanilla Spyder](https://github.com/spyder-ide/spyder)
 
+## Installation
 ```
-unalias spyder
-
-git clone https://github.com/LandonScottBridgewater/Spyder-E/spyder_e
+git clone https://github.com/LandonScottBridgewater/Spyder-E
+cd spyder_e
 pip install --upgrade pip setuptools wheel
 pip install spyder-kernels
 pip install .
-
-spyder_e
 ```
-
 ## Usage
 
 ```
 spyder_e
 ```
+## Motive
 
-# Motive
+I have had trouble with IDEs not giving good session-independence. I'm sure there's probably a way to do this by modifying an existing IDE, but I find that unreliable. I rather have an IDE that I can trust out of the box.
 
-I have had trouble with IDEs not giving good session-idependance. I'm sure there's probably a way to do this by modifying an existing IDE, but I find that unreliable. I rather have an IDE that I can trust.
--- spyder_e/spyder_e/plugins/projects/api.py
+++ spyder_e/spyder_e/plugins/projects/api.py
@@ -69,15 +69,6 @@ class BaseProjectType:
     def set_recent_files(self, recent_files):
         """Set a list of files opened by the project."""
         processed_recent_files = []
-        for recent_file in recent_files:
-            if osp.isfile(recent_file):
-                try:
-                    relative_recent_file = osp.relpath(
-                        recent_file, self.root_path)
-                    processed_recent_files.append(relative_recent_file)
-                except ValueError:
-                    processed_recent_files.append(recent_file)
-
         files = list(OrderedDict.fromkeys(processed_recent_files))
         self.set_option("recent_files", files)
 
@@ -86,12 +77,8 @@ class BaseProjectType:
 
         # Check if recent_files in [main] (Spyder 4)
         recent_files = self.get_option("recent_files", 'main', [])
-        if recent_files:
-            # Move to [workspace] (Spyder 5)
-            self.config.remove_option('main', 'recent_files')
-            self.set_recent_files(recent_files)
-        else:
-            recent_files = self.get_option("recent_files", default=[])
+
+        recent_files = self.get_option("recent_files", default=[])
 
         recent_files = [recent_file if osp.isabs(recent_file)
                         else osp.join(self.root_path, recent_file)
